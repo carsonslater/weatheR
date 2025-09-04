2e91266ea201b0000c115395e87163ef67d0c22f
Carson Slater <carsonslater7@gmail.com>
2025-09-04 00:00:45 -0500

    updated function

--- weatheR.R
+++ weatheR.R
@@ -2,41 +2,29 @@
 #
 # Carson Slater
 # Date Created: 12.16.2024
-# 
+#
 # Description: This is an R script with a function that can print the weather
 # in the console when ran.
 #
 # Check out https://github.com/chubin/wttr.in for more info on `wttr.in`
-# 
+#
 # -------------------------------------------------------------------------
-
 
 # Dependencies ------------------------------------------------------------
 
-library("curl")
+# library("curl")
 
 # Function ----------------------------------------------------------------
 
-
 weather <- function(place = "Waco,TX") {
-  
   stopifnot(is.character(place))
-  
+
   if (curl::has_internet()) {
-  
-    paste0('curl -s "http://wttr.in/', place,'?T" | head -7') |> 
+    paste0('curl -s "http://wttr.in/', place, '?T&u" | head -7') |>
       system()
-    
   } else {
-    
     stop("weather() needs an internet connection to work.")
-    
   }
 }
 
-
-
-
-
-
-
+# weather()
